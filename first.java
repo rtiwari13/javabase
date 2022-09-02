a2350d9d606ab7e0d4abcd00960b5fc6a7d1d1a2
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-09-02 07:15:49 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Main
+{
+        public static void main(String[] args) {
+                System.out.print("Hellow World");
+                System.out.println("I like java");
+                Scanner input = new Scanner(System.in);
+                System.out.println(input.nextInt());
+        }
+}
